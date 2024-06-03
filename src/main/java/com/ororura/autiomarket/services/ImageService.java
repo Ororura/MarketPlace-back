f870ed321e2fbf2201f026d0d1bda107b32978a2
Ororura <hitmanlol890@gmail.com>
2024-06-03 10:58:53 +0300

    fix image repo

--- src/main/java/com/ororura/autiomarket/services/ImageService.java
+++ src/main/java/com/ororura/autiomarket/services/ImageService.java
@@ -3,9 +3,7 @@ package com.ororura.autiomarket.services;
 import com.ororura.autiomarket.repositories.ImageRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ImageService {
@@ -21,7 +19,7 @@ public class ImageService {
 
     @Transactional
     public byte[] getImage(String name) {
-        return this.imageRepo.findByName(name).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found")).getData();
+        return this.imageRepo.findByName(name).get(0).getData();
     }
 
 }
